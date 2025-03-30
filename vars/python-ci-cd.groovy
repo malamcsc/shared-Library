6f75eba38b76e7b9967c69e6d4faff87015042ed
malamcsc <55490652+malamcsc@users.noreply.github.com>
2025-03-30 16:40:10 +0530

    Update python-ci-cd.groovy

--- vars/python-ci-cd.groovy
+++ vars/python-ci-cd.groovy
@@ -13,52 +13,52 @@ def call(body) {
 	    pipelineParams.AWS_REGIONS = 'us-east-1'
 
 
-pipeline {
-
-  agent any
+	pipeline {
 	
-  environment {
-	AWS_REGIONS = "${pipelineParms.AWS_REGIONS}"
-	GIT_CREDENTIAL = "${pipelineParms.GIT_CREDENTIAL}"
-	IMAGE_REPO = "${pipelineParms.IMAGE_REPO}"
-        DOCKERHUB_CREDENTIALS = credentials('dockerhub')
-    }
-
-  stages {
-
-    stage('Checkout Source') {
-      steps {
-        git url:'https://github.com/malamcsc/kubernetes_project.git', branch:'master', credentialsId: 'github'
-      }
-    }
-
-      stage('Checkout') {
-        steps { git branch: 'master', credentialsId: 'github', url: 'https://github.com/malamcsc/kubernetes_project.git'
-        }
-      }
-      stage("Build image") {
-            steps {
-                script {
-                    myapp = docker.build("malamcsc/kubernetes_project:${env.BUILD_ID}")
-                }
-            }
-        }
-    
-         
-        stage('Login and Dcoker push') {
-          steps {
-            script{
-                  withDockerRegistry([ credentialsId: "dockerhub", url: "" ]){
-                  myapp.push("${env.BUILD_ID}") }
-                  }
-		          }
-           }
-         
-    
-    
-
-    
-  }
-
-}
+	  agent any
+		
+	  environment {
+		AWS_REGIONS = "${pipelineParms.AWS_REGIONS}"
+		GIT_CREDENTIAL = "${pipelineParms.GIT_CREDENTIAL}"
+		IMAGE_REPO = "${pipelineParms.IMAGE_REPO}"
+	        DOCKERHUB_CREDENTIALS = credentials('dockerhub')
+	    }
+	
+	  stages {
+	
+	    stage('Checkout Source') {
+	      steps {
+	        git url:'https://github.com/malamcsc/kubernetes_project.git', branch:'master', credentialsId: 'github'
+	      }
+	    }
+	
+	      stage('Checkout') {
+	        steps { git branch: 'master', credentialsId: 'github', url: 'https://github.com/malamcsc/kubernetes_project.git'
+	        }
+	      }
+	      stage("Build image") {
+	            steps {
+	                script {
+	                    myapp = docker.build("malamcsc/kubernetes_project:${env.BUILD_ID}")
+	                }
+	            }
+	        }
+	    
+	         
+	        stage('Login and Dcoker push') {
+	          steps {
+	            script{
+	                  withDockerRegistry([ credentialsId: "dockerhub", url: "" ]){
+	                  myapp.push("${env.BUILD_ID}") }
+	                  }
+			          }
+	           }
+	         
+	    
+	    
+	
+	    
+	  }
+	
+	}
 }
